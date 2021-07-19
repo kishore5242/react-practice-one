d140711660859d3678c21b3c252b692581a8d09b
kishore.anand <kishore.anand@hcl.com>
2021-07-19 20:11:18 +0530

    init

---
+++ fragments-portals-refs/src/App.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./App.css";
+import UserForm from "./components/User/UserForm";
+import UserList from "./components/User/UserList";
+
+const INIT_USERS = [
+  { id: Math.random(), username: "kishore", age: 29 },
+  { id: Math.random(), username: "harish", age: 31 },
+  { id: Math.random(), username: "sandhya", age: 29 },
+];
+
+function App() {
+
+    const [users, setUsers] = useState(INIT_USERS);
+
+    const onAddUserHandler = (user) => {
+      setUsers(previousUsers => [user, ...previousUsers]);
+    }
+
+    return (
+        <div className="app">
+            <UserForm onAddUser={onAddUserHandler}/>
+            <UserList users={users}/>
+        </div>
+    );
+}
+
+export default App;
---
+++ fragments-portals-refs/src/components/User/UserForm.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import Card from "../UI/Card";
+import Modal from "../UI/Modal";
+import styles from "./UserForm.module.css";
+
+function UserForm(props) {
+
+    const [usernameInput, setUsernameInput] = useState('');
+    const [ageInput, setAgeInput] = useState(1);
+
+    const [invalidInput, setInvalidInput] = useState(false);
+
+    const usernameChangeHandler = (event) => {
+        setUsernameInput(event.target.value);
+    }
+
+    const ageChangeHandler = (event) => {
+        setAgeInput(event.target.value);
+    }
+
+    const userFormSubmitHandler = (event) => {
+        event.preventDefault();
+
+        if(usernameInput.trim().length === 0) {
+            setInvalidInput(true);
+            return;
+        } else {
+            setInvalidInput(false);
+        }
+
+        props.onAddUser({id: Math.random(), username: usernameInput, age: ageInput});
+    }
+
+    const modalCancelHandler = () => {
+        setInvalidInput(false);
+    }
+
+    return (
+        <Card>
+            <form onSubmit={userFormSubmitHandler}>
+                <div className={styles['form-control']}>
+                    <label>Username</label>
+                    <input type="text" value={usernameInput} onChange={usernameChangeHandler}/>
+                </div>
+                <div className={styles['form-control']}>
+                    <label>Age (years)</label>
+                    <input type="number" min="1" max="150" step="1" value={ageInput} onChange={ageChangeHandler}/>
+                </div>
+                <button type="submit">Add User</button>
+            </form>
+            {invalidInput && <Modal message="some message" onModalCancel={modalCancelHandler}/>}
+        </Card>
+    );
+}
+
+export default UserForm;
